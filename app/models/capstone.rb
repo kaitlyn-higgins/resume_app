009974839fde26466b376863bd080388654a0b74
paulollerton <paul.ollerton@gmail.com>
2019-07-02 13:25:58 -0700

    adds model validations

--- app/models/capstone.rb
+++ app/models/capstone.rb
@@ -1,5 +1,7 @@
 class Capstone < ApplicationRecord
 
   belongs_to :student
+
+  validates :name, presence: true
   
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -3,4 +3,9 @@ class Student < ApplicationRecord
   has_many :educations
   has_many :skills
   has_many :capstones
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true
+  validates :phone_number, presence: true
 end
