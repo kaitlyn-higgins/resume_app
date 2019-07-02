e5a137619f6a2bfebe2df7858292153bc7c95791
paulollerton <paul.ollerton@gmail.com>
2019-07-02 12:08:25 -0700

    adds capstone controller

---
+++ app/controllers/api/capstones_controller.rb
@@ -0,0 +1,56 @@
+class Api::CapstonesController < ApplicationController
+
+  def index
+    @capstones = Capstone.all
+    render 'index.json.jbuilder'
+  end
+
+  def show
+    @capstone = Capstone.find(params[:id])
+    render 'show.json.jbuilder'
+  end
+
+  def create
+
+    @capstone = Capstone.new(
+      name: params[:name],
+      description: params[:description],
+      url: params[:url],
+      screenshot: params[:screenshot],
+      student_id: params[:student_id],
+      )
+
+    if @capstone.save
+      render 'show.json.jbuilder'
+    else
+      render json: {errors: @capstone.errors.full_messages}, status: :unprocessable_entitiy
+    end
+
+  end
+
+
+  def update
+
+    @capstone = Capstone.find(params[:id])
+
+    @capstone.name = params[:name] || @capstone.name
+    @capstone.description = params[:description] || @capstone.description
+    @capstone.url = params[:url] || @capstone.url
+    @capstone.screenshot = params[:screenshot] || @capstone.screenshot
+    @capstone.student_id = params[:student_id] || @capstone.student_id
+
+    if @capstone.save
+      render 'show.json.jbuilder'
+    else
+      render json: {errors: @capstone.errors.full_messages}, status: :unprocessable_entitiy
+    end
+    
+  end
+
+  def destroy
+    @capstone = Capstone.find(params[:id])
+    @capstone.destroy
+    render json: {message: "Capstone Destroyed"}    
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
-  # EXAMPLE HTML ROUTE
-  # get "/photos" => "photos#index"
 
-  # EXAMPLE JSON ROUTE WITH API NAMESPACE
-  # namespace :api do
-  #   get "/photos" => "photos#index"
-  # end
+  namespace :api do
+
+    get "/capstones" => "capstones#index"
+    get "/capstones/:id" => "capstones#show"
+    post "/capstones" => "capstones#create"
+    patch "/capstones/:id" => "capstones#update"
+    delete "/capstones/:id" => "capstones#destroy"
+
+  end
 end
