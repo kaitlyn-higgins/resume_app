e96492e78b6d94cc208d46efba538b49d16348d0
kaitlyn_higgins <kaitlyn.brooke.higgins@gmail.com>
2019-07-02 12:27:49 -0500

    Creates Student controller, adds index and show

---
+++ app/controllers/api/students_controller.rb
@@ -0,0 +1,13 @@
+class Api::StudentsController < ApplicationController
+
+  def index
+    @students = Student.all
+    render 'index.json.jbuilder'
+  end
+
+  def show
+    @student = Student.find(params[:id])
+    render 'show.json.jbuilder'
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
   # get "/photos" => "photos#index"
 
   # EXAMPLE JSON ROUTE WITH API NAMESPACE
-  # namespace :api do
-  #   get "/photos" => "photos#index"
-  # end
+  namespace :api do
+    get "/students" => "students#index"
+    get "/students/:id" => "students#show"
+  end
 end
