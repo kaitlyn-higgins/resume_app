0b0b02f3cc20e08791fd4e5f78042673d8f92ef0
paulollerton <paul.ollerton@gmail.com>
2019-07-05 10:59:05 -0700

    removes students create action/route

--- app/controllers/api/students_controller.rb
+++ app/controllers/api/students_controller.rb
@@ -1,6 +1,6 @@
 class Api::StudentsController < ApplicationController
 
-  before_action :authenticate_user, only: [:create, :update, :destroy]
+  before_action :authenticate_user, only: [:update, :destroy]
 
   def index
     @students = Student.all
@@ -10,30 +10,6 @@ class Api::StudentsController < ApplicationController
   def show
     @student = Student.find(params[:id])
     render 'show.json.jbuilder'
-  end
-
-  def create
-    @student = Student.new(
-    first_name: params[:first_name],
-    last_name: params[:last_name],
-    email: params[:email],
-    phone_number: params[:phone_number],
-    bio: params[:bio],
-    linked_in: params[:linked_in],
-    twitter: params[:twitter],
-    website: params[:website],
-    resume: params[:resume],
-    github: params[:github],
-    photo: params[:photo],
-    password: params[:password],
-    password_confirmation: params[:password_confirmation],
-    )
-
-    if @student.save
-      render 'show.json.jbuilder'
-    else 
-      render json: {errors: @student.errors.full_messages}, status: :unprocessable_entity
-    end
   end
 
   def update
