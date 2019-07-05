0079854ec254828619ea7a34fb772d688985913d
paulollerton <paul.ollerton@gmail.com>
2019-07-05 09:20:27 -0700

    removes redundant email validation in student model

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,16 +1,14 @@
 class Student < ApplicationRecord
+  
   has_secure_password
-  validates :email, presence: true, uniqueness: true
   has_many :experiences
   has_many :educations
   has_many :skills
   has_many :capstones
 
-<<<<<<< HEAD
-=======
   validates :first_name, presence: true
   validates :last_name, presence: true
-  validates :email, presence: true
+  validates :email, presence: true, uniqueness: true
   validates :phone_number, presence: true
->>>>>>> a00213eda9bd4320bf2589579bdc406b1579034f
+
 end
